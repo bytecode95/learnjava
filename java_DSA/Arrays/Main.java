83dc2d092618d57b296de5b5f4115a7cb901577b
chethanavirajini <cheth.virajini@gmail.com>
2023-10-20 14:14:27 +0530

    java 1D array

---
+++ java_DSA/Arrays/Main.java
@@ -0,0 +1,17 @@
+package Arrays;
+import java.util.Scanner;
+import java.util.Arrays;
+//1_D Array
+public class Main {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        double[] marks = new double[6];
+        for(int i=0; i<marks.length; i++){
+            System.out.print("Enter student "+(i+1)+" mark: ");
+            marks[i] = sc.nextDouble();
+        }
+        sc.close();
+        System.out.println(Arrays.toString(marks));
+
+    }
+}
