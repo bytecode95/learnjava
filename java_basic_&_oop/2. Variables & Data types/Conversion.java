528dcc08b3cdb3483ea664148a25c2e3ddfa81c0
chethanavirajini <cheth.virajini@gmail.com>
2023-10-21 09:55:35 +0530

    typecasting

--- java_basic_&_oop/2. Variables & Data types/Conversion.java
+++ java_basic_&_oop/2. Variables & Data types/Conversion.java
@@ -1,6 +1,6 @@
 //conversion and type casting
 
-public class Main3{
+public class Conversion{
     public static void main(String[]args){
         //byte b = 128; this gives an error int value can not convert into byte. becasue maximum value of byte is 127
 
@@ -8,9 +8,6 @@ public class Main3{
         double b = value;  //conversion
         int k = 35000;
         byte typeCasting = (byte)k;  //typecasting
-
-
-
         System.out.println(value);
         System.out.println(b);  
         System.out.println(typeCasting);
---
+++ java_basic_&_oop/2. Variables & Data types/Typecasting.java
@@ -0,0 +1,9 @@
+class Typecasting{
+    public static void main(String[]args){
+        short s = 1000;
+        byte b = (byte)s;
+        System.out.println(s);
+        System.out.println(b);
+    
+    }
+}
