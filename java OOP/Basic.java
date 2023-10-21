6b3d8cd9fbcaa968595f93f42bd3be4a1b1ec6fc
chethanavirajini <cheth.virajini@gmail.com>
2023-10-21 12:22:06 +0530

    java oop

---
+++ java OOP/Basic.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+//OOP basic classes and object
+public class Basic {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        Student[] students = new Student[5];
+
+        for(int i=0; i<students.length; i++){
+            //object is a instance of a class 
+            students[i] = new Student();
+            System.out.println("Enter "+(i+1)+" name: ");
+            students[i].name = sc.next();
+            System.out.println("Enter "+(i+1)+" address: ");
+            students[i].address = sc.next();
+            System.out.println("Enter "+(i+1)+" age: ");
+            students[i].age = sc.nextInt();
+            System.out.println("Enter "+(i+1)+" mark: ");
+            students[i].mark = sc.nextDouble();
+
+        }
+
+
+
+    }
+}
+
+class Student{
+    //attributes
+    String name;
+    String address;
+    int age;
+    double mark;
+}
