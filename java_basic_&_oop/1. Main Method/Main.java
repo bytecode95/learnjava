e34984ddef612eae5d2747998796aa5f1a5ade8e
chethanavirajini <cheth.virajini@gmail.com>
2023-10-21 13:39:46 +0530

    Static keyword in main method

--- java_basic_&_oop/1. Main Method/Main.java
+++ java_basic_&_oop/1. Main Method/Main.java
@@ -2,7 +2,7 @@
 //class keyowrd is used to declare in class in java
 // public is access specifier and 
 // static keyowrd specify the main method can invoke without creating an object
-
+//main method should be static 
 
 class Main{
     public static void main(String[] args){
