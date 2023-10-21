fe6135354cfe6a9c348760bfce9a11a4281588a8
chethanavirajini <cheth.virajini@gmail.com>
2023-10-21 14:26:34 +0530

    javac

---
+++ java_basic_&_oop/1. Main Method/Program.java
@@ -0,0 +1,8 @@
+//java compiler complied this code and converted into bytecode .class file.
+// this .class file can be only executed by JVM.
+
+public class Program {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
