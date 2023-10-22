165da3a59a0d3c736a163275017fb4951f3324de
chethanavirajini <cheth.virajini@gmail.com>
2023-10-22 19:58:34 +0530

    Constructor

---
+++ java OOP/Constructor.java
@@ -0,0 +1,69 @@
+//Constructor 
+
+//a constructor is a block of codes similar to the method. It is called when an instance of the class is created. At the time of calling constructor, memory for the object is allocated in the memory.
+
+
+public class Constructor {
+    public static void main(String[] args){
+        Vehicle v1 = new Vehicle();
+		v1.brandName = "Toyota Corolla";
+		v1.yearofManufacturing = 2002;
+		v1.transmission = "Auto";
+		v1.isairbagavaiable = true;
+		
+		System.out.println(v1.brandName+" "+v1.yearofManufacturing);
+		
+		Vehicle v2 = new Vehicle();
+		v2.brandName = "Tesla";
+		//v2.yearofManufacturing = 2007;
+		v2.transmission = "Auto";
+		v2.isairbagavaiable = true;
+		
+		System.out.println(v2.brandName+" "+v2.yearofManufacturing);
+		
+		
+		Vehicle v3 = new Vehicle("Jeep", 2016, "Manual", true);
+		System.out.println(v3.brandName);
+
+    }
+}
+
+class Vehicle{
+	//attributes
+	//static keyword usage
+	String brandName;
+	static int yearofManufacturing;
+	String transmission;
+	boolean isairbagavaiable;
+	
+	//constructor --- default constructor
+	public Vehicle(){
+		System.out.println("Now Vehicle Object is creating  - default constructor");
+	}
+	
+	//method overloading - methods can be defined agin when parameter type or count changing
+	//constructor -- parameterized constructor
+	
+	public Vehicle(String n, int y, String t, boolean a){
+		System.out.println("Now Vehicle Object is creating - parameterized constructor");
+		brandName = n;
+		yearofManufacturing = y;
+		transmission = t;
+		isairbagavaiable = a;
+	
+	}
+	
+	//method or action
+	//static method
+	
+	public static void start(){
+		//stop(); error
+	}
+	
+	
+	//non static method
+	public void stop(){
+		start();
+		
+	}
+}
