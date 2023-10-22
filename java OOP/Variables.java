d5452d3f68155054406f8ce84569c04e77a144d1
chethanavirajini <cheth.virajini@gmail.com>
2023-10-22 20:52:34 +0530

    Varibles

---
+++ java OOP/Variables.java
@@ -0,0 +1,56 @@
+//3 variables types  1. Local varibles 2. Instance variables 3. Static Varibles
+
+public class Variables {
+    public static void main(String[] args){
+    
+		Vehicle v1 = new Vehicle("Jeep", 2016, "Manual", true);
+		System.out.println(v1.brandName);
+
+    }
+}
+
+class Vehicle{
+	//instance varaibles - have default values
+    //string - null
+    //int - 0
+    //double - 0.0
+    //boolean - false
+	String brandName;
+    //static variable
+	static int yearofManufacturing;
+	String transmission;
+	boolean isairbagavaiable;
+
+	public Vehicle(){
+		System.out.println("Now Vehicle Object is creating  - default constructor");
+	}
+	
+	public Vehicle(String brandName, int year, String transmissiontype, boolean air){
+		System.out.println("Now Vehicle Object is creating - parameterized constructor");
+		brandName = brandName;
+		yearofManufacturing = year;
+		transmission = transmissiontype;
+		isairbagavaiable = air;
+	
+	}
+
+	//local varibles --- inside a method 
+    //no default value
+    //local varibles can not define outside a method
+    //varible declaration can be done , but when we are using local varibles should have a values. other wise error
+	public static void start(){
+        int speed;
+        speed = 10;
+        System.out.println(speed);
+		
+	}
+	
+
+	public void stop(){
+		start();
+		
+	}
+	
+	
+	
+}
