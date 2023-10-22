59ca2556b8d150f963cc4a1fe5fe7272ecca5ee9
chethanavirajini <cheth.virajini@gmail.com>
2023-10-22 20:34:35 +0530

    This Keyword

--- java OOP/This.java
+++ java OOP/This.java
@@ -1,9 +1,6 @@
-//Constructor 
+//this keyword -- this is a reference variable that refers to the current object
 
-//a constructor is a block of codes similar to the method. It is called when an instance of the class is created. At the time of calling constructor, memory for the object is allocated in the memory.
-
-
-public class Constructor {
+public class This {
     public static void main(String[] args){
         Vehicle v1 = new Vehicle();
 		v1.brandName = "Toyota Corolla";
@@ -26,13 +23,15 @@ public class Constructor {
 		System.out.println(v3.brandName);
 
     }
+    
 }
+
 
 class Vehicle{
 	//attributes
 	//static keyword usage
 	String brandName;
-	static int yearofManufacturing;
+	int yearofManufacturing;
 	String transmission;
 	boolean isairbagavaiable;
 	
@@ -44,16 +43,15 @@ class Vehicle{
 	//method overloading - methods can be defined agin when parameter type or count changing
 	//constructor -- parameterized constructor
 	
-	public Vehicle(String name, int year, String transmissiontype, boolean airbagavaiablity){
+	public Vehicle(String brandName, int yearofManufacturing, String transmission, boolean isairbagavaiabler){
 		System.out.println("Now Vehicle Object is creating - parameterized constructor");
-        //varaible = parameter name 
-        //dont use same name for variable and parameter name
-		brandName = name;
-		yearofManufacturing = year;
-		transmission = transmissiontype;
-		isairbagavaiable = airbagavaiablity;
+		this.brandName = brandName;
+		this.yearofManufacturing = yearofManufacturing;
+		this.transmission =  transmission;
+		this.isairbagavaiable = isairbagavaiabler;
 	
 	}
+	
 	
 	//method or action
 	//static method
@@ -68,4 +66,7 @@ class Vehicle{
 		start();
 		
 	}
+	
+	
+	
 }
