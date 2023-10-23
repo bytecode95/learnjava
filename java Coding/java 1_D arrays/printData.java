ae2fe4f7181d22e2f62942d1b230f5b92449c20f
chethanavirajini <cheth.virajini@gmail.com>
2023-10-23 19:25:06 +0530

    print data

---
+++ java Coding/java 1_D arrays/printData.java
@@ -0,0 +1,17 @@
+
+
+public class printData {
+    public static void main(String[] args){
+        int[] ar = new int[5];
+        //every element of ar array is 0
+        for(int i=0; i<ar.length; i++){
+            System.out.println(ar[i]);
+        }
+        //How to print every element in an array this is O(n) time complexity
+        int[] numbers = {25, 78, 96, 54};
+        for(int i=0; i<numbers.length; i++){
+            System.out.println(numbers[i]);
+
+        }
+    }
+}
