fee34596b526cb59250d5f7415c49a96b7b5d5ab
chethanavirajini <cheth.virajini@gmail.com>
2023-10-30 13:09:44 +0530

    ReverseNumber

--- java_Pogramming/NumberPrograms/ReverseNumber.java
+++ java_Pogramming/NumberPrograms/ReverseNumber.java
@@ -1,5 +1,16 @@
 public class ReverseNumber {
     public static void main(String[] args){
+        int a = 12345;
+        int n = 0;
+        int reverseNumber = 0;
+        while(a>0){
+           n = a % 10;
+           reverseNumber = reverseNumber*10 + n;
+           
+           a = a/10;
+          
+        }
+        System.out.print(reverseNumber);
 
     }
     
