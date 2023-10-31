b35d218fed9ac6fb990d601e1b0ca912c72887d8
chethanavirajini <cheth.virajini@gmail.com>
2023-10-31 13:13:24 +0530

    copy

---
+++ java_Pogramming/Arrays/Basicarray.java
@@ -0,0 +1,14 @@
+package Arrays;
+import java.util.Arrays;
+public class Basicarray {
+    public static void main(String[] args){
+        //defined array
+        int[] num1 = {23, 45, 36, 48, 98, 76};
+        int[] num2 = new int[num1.length];
+
+        for(int i=0; i<num2.length; i++){
+            num2[i] = num1[1];
+        }
+        System.out.println(Arrays.toString(num2));
+    }
+}
