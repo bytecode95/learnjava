9de44a3e1462af483ce016059e562837c55b638f
chethanavirajini <cheth.virajini@gmail.com>
2023-10-31 13:53:17 +0530

    print even & odd

--- java_Pogramming/Arrays/Printodd.java
+++ java_Pogramming/Arrays/Printodd.java
@@ -1,10 +1,10 @@
 package Arrays;
 
-public class Printeven {
+public class Printodd {
     public static void main(String[] args){
         int[] num = {23, 45, 47, 33, 78, 56, 97};
         for(int i=0; i< num.length; i++){
-            if(i%2!=0){
+            if(i%2==0){
                 continue;
             }else{
                 System.out.println(num[i]);
