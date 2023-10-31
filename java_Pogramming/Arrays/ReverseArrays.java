44cc460a69e902f4b059933d5c479dbe5c037088
chethanavirajini <cheth.virajini@gmail.com>
2023-10-31 13:25:39 +0530

    ReverseArray

---
+++ java_Pogramming/Arrays/ReverseArrays.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class ReverseArrays {
+    public static void main(String[] args){
+        int[] num = {34, 56, 27, 58, 49, 86, 59, 74};
+        for(int i=num.length-1; i>=0; i--){
+            System.out.println(num[i]);
+        }
+    }
+}
