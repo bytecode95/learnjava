50dca40ad62b5062dad4113ed3eae7ad13a57e33
chethanavirajini <cheth.virajini@gmail.com>
2023-11-01 23:40:28 +0530

    polymorphsim

---
+++ java MainOOP concepts/PolymorpshismOOP.java
@@ -0,0 +1,58 @@
+
+//Dynamic Polymorphism 
+public class PolymorpshismOOP {
+    public static void main(String[] args){
+        Animal dog = new Dog();
+        dog.makeSound();
+
+        Animal cat = new Cat();
+        cat.makeSound();
+
+    }
+}
+
+class Animal{
+    String name;
+    int age;
+
+
+    void Eat(){
+
+    }
+
+    void Sleeping(){
+
+    }
+
+    void makeSound(){
+
+    }
+}
+
+class Dog extends Animal{
+    String type;
+    String color;
+
+    void run(){
+
+    }
+
+    @Override
+    void makeSound(){
+        System.out.println("Dog Barking");
+    }
+}
+
+class Cat extends Animal{
+    String type;
+    String color;
+
+    void run(){
+
+    }
+
+    @Override
+    void makeSound(){
+        System.out.println("Cat Meow Meow");
+    }
+}
