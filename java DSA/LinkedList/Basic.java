495e3acf77e74125072aa3162a1189d6930c3158
chethanavirajini <cheth.virajini@gmail.com>
2023-11-02 11:15:54 +0530

    LinkedList

---
+++ java DSA/LinkedList/Basic.java
@@ -0,0 +1,15 @@
+//creating linked list
+//insertting value at the end 
+
+
+
+public class Basic{
+   public static void main(String[] args){
+    LinkedList list = new LinkedList();
+    list.insert(5);
+    list.insert(45);
+    list.insert(12);
+    list.show();
+
+   }
+}
