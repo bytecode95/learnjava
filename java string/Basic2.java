954957fabc458ed86abea6769f2ea22018945089
chethanavirajini <cheth.virajini@gmail.com>
2023-11-09 10:19:32 +0530

    toString Method

---
+++ java string/Basic2.java
@@ -0,0 +1,9 @@
+public class Basic2 {
+    public static void main(String[] args){
+        System.out.println(56);
+        //this method is deprciated- just for demo
+        Integer num = new Integer(56);
+        System.out.println(num.toString());
+    
+    }
+}
