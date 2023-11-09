0ecf7bf4e5c0f3b220d69af5eda54fb4faf1d613
chethanavirajini <cheth.virajini@gmail.com>
2023-11-09 10:11:43 +0530

    Basic

--- java_basic_&_oop/1. Main Method/Program.java
+++ java_basic_&_oop/1. Main Method/Program.java
@@ -1,8 +1,19 @@
 //java compiler complied this code and converted into bytecode .class file.
 // this .class file can be only executed by JVM.
 
+import java.util.Arrays;
+
 public class Program {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        //here println function overloading
+        System.out.println(56);
+        String name = "chethana";
+        System.out.println(name);
+        System.out.println('A');
+        int[] nums = {2,4,6,7};
+        //function overriding -- here toString method is over-riding use toString method in Arrays.util Pacakge
+        System.out.println(nums);   //[I@49e4cb85
+        System.out.println(Arrays.toString(nums));  //[2, 4, 6, 7]
     }
 }
