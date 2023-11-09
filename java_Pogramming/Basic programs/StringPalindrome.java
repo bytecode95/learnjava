210301689de12562b6d0f834a287384f1efde337
chethanavirajini <cheth.virajini@gmail.com>
2023-11-09 11:41:00 +0530

    String palindrome

---
+++ java_Pogramming/Basic programs/StringPalindrome.java
@@ -0,0 +1,19 @@
+public class StringPalindrome {
+    public static void main(String[] args){
+        String str = "";
+        System.out.println(isPalindrome(str));
+
+    }
+
+    static boolean isPalindrome(String str){
+        if(str == null || str.length()==0){
+            return true;
+        }
+        for(int i=0; i<str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(str.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
